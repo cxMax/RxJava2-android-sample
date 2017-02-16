2850400cc4520fd1ffefd9224e6e739c3cfa7288
cxmax <cxmax@github.com>
2017-02-17 00:30:15 +0800

    Completable操作符练习
    Completable操作符练习

---
+++ app/src/main/java/com/cxmax/rxjava2_android_sample/ui/operators/CompletableObserverExampleActivity.java
@@ -0,0 +1,78 @@
+package com.cxmax.rxjava2_android_sample.ui.operators;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.cxmax.rxjava2_android_sample.R;
+import com.cxmax.rxjava2_android_sample.utils.AppConstant;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by cxmax on 2017/2/16.
+ */
+
+public class CompletableObserverExampleActivity extends AppCompatActivity {
+
+    private static final String TAG = CompletableObserverExampleActivity.class.getSimpleName();
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+        btn = (Button) findViewById(R.id.btn);
+        textView = (TextView) findViewById(R.id.textView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doSomeWork();
+            }
+        });
+    }
+
+    private void doSomeWork() {
+        Completable completable = Completable.timer(1000, TimeUnit.MILLISECONDS);
+
+        completable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(getCompletableObserver());
+
+    }
+
+    private CompletableObserver getCompletableObserver() {
+        return new CompletableObserver() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, " onSubscribe : " + d.isDisposed());
+            }
+
+            @Override
+            public void onComplete() {
+                textView.append(" onComplete");
+                textView.append(AppConstant.LINE_SEPARATOR);
+                Log.d(TAG, " onComplete");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                textView.append(" onError : " + e.getMessage());
+                textView.append(AppConstant.LINE_SEPARATOR);
+                Log.d(TAG, " onError : " + e.getMessage());
+            }
+        };
+    }
+}
